7b90d82108044b0a19bcc700272e55b2ef9a59ef
Felipe Lima <felipedesouzamali@hotmail.com>
2022-03-30 10:23:13 -0400

    alteracoes no componente Home

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,35 +1,41 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "../../components/Menu";
-import ListWatch from "../../components/ListWatch";
 import InfoAnime from "../../components/InfoAnime";
+import Footer from "../../components/Footer";
+import { BackTop } from "antd";
+import "./index.css";
+import ContentStart from "../ContentStart";
+import AuthProvider from "../../providers/auth";
 
 const Home = () => {
-  const [imgs, setImgs] = useState([]);
-  const [nameAnime, setNameAnime] = useState();
-  useEffect(() => {
-    axios
-      .get(`https://api.jikan.moe/v4/anime?q=${nameAnime}`)
-      .then((response) => {
-        const result = response.data.data.map((e) => {
-          return {
-            url: e.images,
-          };
-        });
-        // setImgs(result);
-        setImgs(response.data.data);
-        console.log(response.data.data[0].trailer.embed_url);
-      });
-  }, [nameAnime]);
-  console.log(imgs);
-  console.log(nameAnime);
+  const style = {
+    height: 40,
+    width: 40,
+    lineHeight: "40px",
+    borderRadius: 4,
+    backgroundColor: "#3f7a7e",
+    color: "#fff",
+    textAlign: "center",
+    fontSize: 14,
+  };
 
   return (
-    <>
-      <Menu nameAnime={setNameAnime} />
-      {nameAnime && <ListWatch imgs={imgs} />}
-      {nameAnime && <InfoAnime imgs={imgs} />}
-    </>
+    <BrowserRouter>
+      <div className="home">
+        <AuthProvider>
+          <Menu />
+          <Routes>
+            <Route exact path="/" element={<ContentStart />} />
+            <Route exact path="/sinopse" element={<InfoAnime />} />
+          </Routes>
+        </AuthProvider>
+
+        <BackTop>
+          <div style={style}>Topo</div>
+        </BackTop>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 };
 
