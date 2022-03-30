57e9dc47f680472b95623616630c8f16c3d5ae10
Felipe Lima <felipedesouzamali@hotmail.com>
2022-03-30 10:21:22 -0400

    alteracoes no componente Menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,51 +1,57 @@
 import { Card } from "antd";
 import "antd/dist/antd.css";
 import "./index.css";
-import { Row, Col, Divider } from "antd";
 import Logo from "../../img/logo.png";
-import { SearchOutlined, UserAddOutlined } from "@ant-design/icons";
+import {
+  SearchOutlined,
+  UserAddOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
 import ButtonsDrop from "../ButtonsDropDown";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { AuthContext } from "../../providers/auth";
 
 const { Meta } = Card;
 
-const Menu = ({ nameAnime }) => {
-  const [valueInput, setValueInput] = useState();
+const Menu = () => {
+  const { menuSx, setNameAnime } = useContext(AuthContext);
+
+  const [valueInput, setValueInput] = useState("");
 
   return (
-    <div className="menu">
-      <img className="menu-logo" src={Logo} alt="" />
+    <>
+      <div className="menu">
+        <img className="menu-logo" src={Logo} alt="" />
 
-      <div className="menu-buttons">
-        <ButtonsDrop />
-
-        {/* <button className="menu-buttons-center">animes</button>
-        <button className="menu-buttons-center">Filmes</button> */}
-        {/* <button className="menu-buttons-center">Leitor</button> */}
-        {/* <button className="menu-buttons-center">Hentais</button> */}
-        {/* <button className="menu-buttons-center">Calendários</button> */}
-        {/* <button className="menu-buttons-center">Animes ON</button> */}
+        <div className="menu-buttons">
+          <ButtonsDrop />
+        </div>
+        <div className="div-menu-input">
+          <Link to={"/sinopse"}>
+            <SearchOutlined
+              onClick={() => {
+                setNameAnime(valueInput);
+                setValueInput("");
+              }}
+              className="icon-search"
+            />
+          </Link>
+          <input
+            value={valueInput}
+            onChange={(e) => setValueInput(e.target.value)}
+            className="menu-input"
+            type="text"
+            placeholder="Buscar por Animes"
+          />
+        </div>
+        <div className="menu-icons">
+          <UserAddOutlined className="icons" />
+          <MenuOutlined className="menu-mobilee" onClick={menuSx} />
+        </div>
       </div>
-      <div className="div-menu-input">
-        <input
-          value={valueInput}
-          onChange={(e) => setValueInput(e.target.value)}
-          className="menu-input"
-          type="text"
-          placeholder="Buscar por Animes"
-        />
-        <SearchOutlined
-          onClick={() => {
-            nameAnime(valueInput);
-            setValueInput("");
-          }}
-          className="icon-search"
-        />
-      </div>
-      <div className="menu-icons">
-        <UserAddOutlined className="icons" />
-      </div>
-    </div>
+    </>
   );
 };
 
